84b1f4d43bde52d631747b2ea6e62b94f57a4966
joncarsel <joncarsel@gmail.com>
2020-06-19 05:14:16 +0300

    first commit fron vs code

---
+++ app.js
@@ -0,0 +1,37 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+const dhr = document.querySelector('#dhr');
+const dmn = document.querySelector('#dmn');
+const dsc = document.querySelector('#dsc');
+const dv = document.querySelector('#div');
+
+setInterval(() => {
+    let day = new Date();
+
+    let dhh = day.getHours();
+    let dmm = day.getMinutes();
+    let dss = day.getSeconds();
+
+
+
+    let hh = dhh * 30;
+    let mm = dmm * deg;
+    let ss = dss * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+
+    dhr.innerHTML = `${dhh}`;
+    dmn.innerHTML = `${dmm}`;
+    dsc.innerHTML = `${dss}`;
+
+    dsc.style.color = `#${dhh}${dmm}${dss*2}`;
+    dmn.style.color = `#${dss*2}${dhh}${dmm}`;
+    dhr.style.color = `#${dmm}${dss*2}${dhh}`;
+
+    dv.style.width += `${dss}*5`;
+});
